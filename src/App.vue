a1ccd51a5dfda9124827ed848afe1b6f68cea12e
sashank-gl <glsashank365@gmail.com>
2024-04-05 19:08:30 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <ScrollToTop />
+    <div class="text-9xl flex justify-center items-center text-white bg-indigo-500 h-screen">Top</div>
+    <div class="text-9xl flex justify-center items-center text-white bg-rose-500 h-screen">Middle</div>
+    <div class="text-9xl flex justify-center items-center text-white bg-purple-500 h-screen">Bottom</div>
+</template>
+
+<script setup>
+import ScrollToTop from './components/ScrollToTop.vue'
+</script>
---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,54 @@
+<template>
+  <Transition>
+    <a
+      v-if="isVisible"
+      href="#"
+      class="fixed bottom-4 right-4 bg-white text-indigo-500 p-2 rounded-full"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="size-6"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z"
+          clip-rule="evenodd"
+        />
+      </svg> </a
+  ></Transition>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+const isVisible = ref(false);
+
+const handleScroll = () => {
+  if (window.pageYOffset > 500) {
+    isVisible.value = true;
+  } else {
+    isVisible.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
